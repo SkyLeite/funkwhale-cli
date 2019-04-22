4a914beb1ed819b80d467ec7ed46c2547125af3b
Rodrigo Leite <rodrigolf.dev@gmail.com>
2019-04-22 20:39:40 -0300

    Add option to recursively crawl directory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use structopt::StructOpt;
+use walkdir::WalkDir;
 
 mod upload;
 
@@ -24,6 +25,9 @@ enum Opt {
 
         #[structopt(short = "m", long = "timeout", default_value = "500")]
         timeout: u64,
+
+        #[structopt(short = "d", long = "depth", default_value = "5")]
+        max_depth: u64,
     },
 }
 
@@ -32,31 +36,35 @@ fn parse_token_file(path: std::path::PathBuf) -> String {
     return file.trim().to_string();
 }
 
-fn parse_file(file: std::path::PathBuf) -> Vec<std::path::PathBuf> {
+fn parse_file(file: std::path::PathBuf, max_depth: u64) -> Vec<std::path::PathBuf> {
     if !file.exists() {
         panic!("File or directory doesn't exist");
     }
 
     if file.is_dir() {
-        return std::fs::read_dir(file)
-            .unwrap()
-            .map(|res| res.unwrap().path())
+        let files: Vec<std::path::PathBuf> = WalkDir::new(file)
+            .follow_links(true)
+            .max_depth(max_depth as usize)
+            .into_iter()
+            .map(|f| f.ok().unwrap().path().to_path_buf())
+            .filter(|f| f.is_file())
             .collect();
+
+        return files;
     } else {
         return vec![file];
     }
-
 }
 
 fn main() {
     let args = Opt::from_args();
 
-    if let Opt::Upload { interactive, file, library, instance_url, token_file, timeout } = args {
+    if let Opt::Upload { interactive, file, library, instance_url, token_file, timeout, max_depth } = args {
         let token = parse_token_file(token_file);
-        let all_files = parse_file(file);
+        let all_files = parse_file(file, max_depth);
 
         match upload::main(all_files, library, instance_url, token, interactive, timeout) {
-            Ok(v) => println!("\nUpload successful!"),
+            Ok(_v) => println!("\nUpload successful!"),
             Err(e) => panic!("{}", e),
         }
     }
