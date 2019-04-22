a8d5dfa674acf0fbafccf0c9a3f3b0ac2a1cbafd
Rodrigo Leite <rodrigolf.dev@gmail.com>
2019-04-22 10:55:57 -0300

    Add support for directory uploads

--- src/funkwhale.rs
+++ src/funkwhale.rs
@@ -32,29 +32,37 @@ pub fn get_libraries(instance: &str, token: &str) -> Result<Vec<Library>, Box<st
 }
 
 pub fn upload(files: Vec<std::path::PathBuf>, library: String, instance: String, token: String, timeout: u64) -> Result<(), Box<std::error::Error>> {
-    let filename = &files[0].file_name().unwrap().to_str().unwrap();
     let now = Utc::now();
+    let import_reference = format!("From CLI at {}", now);
+    let url = format!("{}/api/v1/uploads/", instance);
+    let total_files = &files.len();
+    let mut current_file = 0;
 
-    let form = multipart::Form::new()
-        .text("library", library)
-        .text("import_reference", format!("From CLI at {}", now))
-        .text("source", format!("upload://{}", filename))
-        .file("audio_file", &files[0])?;
+    for file in &files {
+        let filename = file.file_name().unwrap().to_str().unwrap();
 
-    let sp = Spinner::new(Spinners::Moon, "Uploading your files. Please wait.".into());
-    let client = reqwest::Client::builder()
-        .timeout(std::time::Duration::from_secs(timeout))
-        .build()?;
+        let form = multipart::Form::new()
+            .text("library", library.clone())
+            .text("import_reference", import_reference.clone())
+            .text("source", format!("upload://{}", filename))
+            .file("audio_file", file)?;
 
-    let _resp = client
-        .post(&format!("{}/api/v1/uploads/", instance))
-        .bearer_auth(token)
-        .multipart(form)
-        .send()?
-        .text()?;
+        let sp = Spinner::new(Spinners::Moon, "Uploading your files. Please wait.".into());
+        let client = reqwest::Client::builder()
+            .timeout(std::time::Duration::from_secs(timeout))
+            .build()?;
 
-    sp.message("Uploaded!".into());
-    sp.stop();
+        let _resp = client
+            .post(&url)
+            .bearer_auth(token.clone())
+            .multipart(form)
+            .send()?
+            .text()?;
+
+        sp.message(format!("Uploaded file {} of {}. Uploading...", &current_file + 1, &total_files));
+        sp.stop();
+        current_file += 1;
+    }
 
     Ok(())
 }
